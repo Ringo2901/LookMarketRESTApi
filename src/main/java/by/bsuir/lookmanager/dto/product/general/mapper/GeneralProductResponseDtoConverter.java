d58710bcb22a1508240bc88a2ab621b9ff82944a
Ringo2901 <nekitale@yandex.ru>
2024-04-07 23:00:13 +0300

    fix Search query converter

--- src/main/java/by/bsuir/lookmanager/dto/product/general/mapper/GeneralProductResponseDtoConverter.java
+++ src/main/java/by/bsuir/lookmanager/dto/product/general/mapper/GeneralProductResponseDtoConverter.java
@@ -7,25 +7,26 @@ import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Map;
 
 @Component
-public class GeneralProductResponseDtoConverter implements Converter<List<Object>, GeneralProductResponseDto> {
+public class GeneralProductResponseDtoConverter implements Converter<Map<String, Object>, GeneralProductResponseDto> {
 
     @Override
-    public GeneralProductResponseDto convert(List<Object> result) {
+    public GeneralProductResponseDto convert(Map<String, Object> result) {
         GeneralProductResponseDto dto = new GeneralProductResponseDto();
-        dto.setId((Long) result.get(0));
-        dto.setStatus(ProductStatus.valueOf((String) result.get(1)));
-        dto.setTitle((String) result.get(2));
-        dto.setPrice((Double) result.get(3));
-        dto.setCreatedTime((Timestamp) result.get(4));
-        dto.setUpdateTime((Timestamp) result.get(5));
-        dto.setSubCategoryName((String) result.get(6));
-        dto.setCategoryName((String) result.get(7));
-        dto.setUserId((Long) result.get(8));
-        dto.setLogin((String) result.get(9));
-        dto.setImageId((Long) result.get(10));
-        dto.setImageData((String) result.get(11));
+        dto.setId((Long) result.get("id"));
+        dto.setStatus(ProductStatus.valueOf((String) result.get("status")));
+        dto.setTitle((String) result.get("title"));
+        dto.setPrice((Double) result.get("price"));
+        dto.setCreatedTime((Timestamp) result.get("createdTime"));
+        dto.setUpdateTime((Timestamp) result.get("updateTime"));
+        dto.setSubCategoryName((String) result.get("subcategoryname"));
+        dto.setCategoryName((String) result.get("categoryname"));
+        dto.setUserId((Long) result.get("userid"));
+        dto.setLogin((String) result.get("login"));
+        dto.setImageId((Long) result.get("imageid"));
+        dto.setImageData((String) result.get("imagedata"));
         return dto;
     }
 }
