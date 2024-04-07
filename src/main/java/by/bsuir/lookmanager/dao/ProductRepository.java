12776ecec3f617d356fea6e17dd4500ce61d8621
Ringo2901 <nekitale@yandex.ru>
2024-04-07 16:04:01 +0300

    fix Search query

--- src/main/java/by/bsuir/lookmanager/dao/ProductRepository.java
+++ src/main/java/by/bsuir/lookmanager/dao/ProductRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 
 import java.util.List;
@@ -14,9 +15,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>, JpaSpecificationExecutor<ProductEntity> {
     Page<ProductEntity> findAll(Specification<ProductEntity> spec, Pageable pageable);
 
-    @Procedure(name = "get_products")
-    List<GeneralProductResponseDto> get_products(String query, Integer pageSize, Integer pageNumber, String sortBy, String sortOrder,
-                                                 Integer[] size, String[] color, String brand, String[] filtSeason, String[] filtGender,
-                                                 String[] filtAgeType, String[] tags, String[] materials, String[] subcategory, String[] category,
-                                                 Double minPrice, Double maxPrice);
+    @Query(value = "SELECT * FROM get_products(:query, :pageSize, :pageNumber, :sortBy, :sortOrder, :size, :color, :brand, :filtSeason, :filtGender, :filtAgeType, :tags, :materials, :subcategory, :category, :minPrice, :maxPrice)", nativeQuery = true)
+    List<GeneralProductResponseDto> getProducts(String query, int pageSize, int pageNumber, String sortBy, String sortOrder, Integer[] size, String[] color, String brand, String[] filtSeason, String[] filtGender, String[] filtAgeType, String[] tags, String[] materials, String[] subcategory, String[] category, double minPrice, double maxPrice);
+
 }
