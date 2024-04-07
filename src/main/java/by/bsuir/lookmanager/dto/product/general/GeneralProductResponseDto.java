56d50571b96cf733906bd9621d7ccda44132a71d
Ringo2901 <nekitale@yandex.ru>
2024-04-07 22:38:33 +0300

    fix Search query converter

--- src/main/java/by/bsuir/lookmanager/dto/product/general/GeneralProductResponseDto.java
+++ src/main/java/by/bsuir/lookmanager/dto/product/general/GeneralProductResponseDto.java
@@ -1,10 +1,13 @@
 package by.bsuir.lookmanager.dto.product.general;
 
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
+import java.util.Base64;
 
 @Data
 @AllArgsConstructor
@@ -12,7 +15,10 @@ import java.sql.Timestamp;
 public class GeneralProductResponseDto {
     private Long id;
     private String title;
+    @Temporal(TemporalType.TIMESTAMP)
     private Timestamp createdTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
     private Timestamp updateTime;
     private Double price;
     private String subCategoryName;
@@ -24,4 +30,13 @@ public class GeneralProductResponseDto {
     private Long imageId;
     private String imageData;
     //private boolean isFavourite;
+
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = Base64.getEncoder().encodeToString(imageData);;
+    }
+
+    public void setImageData(String imageData) {
+        this.imageData = imageData;;
+    }
 }
